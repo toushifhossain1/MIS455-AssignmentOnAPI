0530c0c0344008d4d06a75367adc70c50f585c4c
toushifhossain1 <1920404@iub.edu.bd>
2021-12-20 02:36:07 +0600

    commit message

--- API.js
+++ API.js
@@ -1,11 +1,13 @@
 var url = `https://www.themealdb.com/api/json/v1/1/categories.php`;
 
+callAPI();
+
 function callAPI() {
     fetch(url)
         .then(res => res.json())
         .then(data => card(data.categories))
 }
-callAPI();
+
 
 function card(data) {
     //console.log(data);
@@ -13,20 +15,73 @@ function card(data) {
     var newDiv = document.getElementById("div");
     for (var i = 0; i < data.length; i++) {
         //console.log(i);
-        console.log(data[i].strCategory);
+
+        //console.log(data[i].strCategory);
         var category = (data[i].strCategory);
         var details = (data[i].strCategoryDescription);
         var image = (data[i].strCategoryThumb);
         var newDiv = document.createElement("div");
+        //newStyle.classList.add("styles");
 
-        newDiv.innerHTML = `<div class="card" style="width: 18rem;">
-            <img src=" ${image}" class="card-img-top" alt="">
-            <div class="card-body">
-                <h5 class="card-title" id="catagory"> <p> ${category} <br></p> </h5>
-                <p class="card-text" id="description"><p> ${details} <br></p> </p>
+
+
+
+
+        newDiv.innerHTML = `<div class="card" style="width: 1000px;">
+            <img src=" ${image}" class="card-img-top" alt=""  >
+            <div class="card-body ">
+                <h5 class="card-title" id="catagory" >  ${category} <br> </h5>
+                <p class="card-text" id="description"> ${details} <br> 
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div> `;
         content.appendChild(newDiv);
     }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function searchResults() {
+
+    var search;
+    search = document.getElementById("searchbox").value;
+
+
+
+
 }
