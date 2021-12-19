9686d0d22ebde440a9f40dece373e8b746b78079
toushifhossain1 <1920404@iub.edu.bd>
2021-12-20 00:40:39 +0600

    commit message

---
+++ API.js
@@ -0,0 +1,30 @@
+var url = `https://www.themealdb.com/api/json/v1/1/categories.php`;
+
+function callAPI() {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => card(data.categories))
+}
+callAPI();
+
+function card(data) {
+    //console.log(data);
+    var content = document.getElementById("BootStrapData");
+    var newDiv = document.getElementById("div");
+    for (var i = 0; i < data.length; i++) {
+        //console.log(i);
+        console.log(data[i].strCategory);
+        var category = (data[i].strCategory);
+        var details = (data[i].strCategoryDescription);
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = `<div class="card" style="width: 18rem;">
+            <img src="" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title" id="catagory"> <p> ${category} <br></p> </h5>
+                <p class="card-text" id="description"><p> ${details} <br></p> </p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div> `;
+        content.appendChild(newDiv);
+    }
+}
